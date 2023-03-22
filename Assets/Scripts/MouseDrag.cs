d3848a5dc7eb884448a8ef53072b427bf6548fce
Terryn <tg3213@gmail.com>
2023-03-22 11:21:12 -0600

    Adds snap point prefab and functionality

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -10,76 +10,73 @@ public class MouseDrag : MonoBehaviour
 {
 
     private Vector3 mOffset;
-
-
     private float mZCoord;
 
 
-    void OnMouseDown()
+    void OnMouseDown(){
 
-    {
-
-        mZCoord = Camera.main.WorldToScreenPoint(
-
-            gameObject.transform.position).z;
-
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 
         // Store offset = gameobject world pos - mouse world pos
 
         mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-
     }
 
 
-    private Vector3 GetMouseAsWorldPoint()
-
-    {
+    private Vector3 GetMouseAsWorldPoint(){
 
         // Pixel coordinates of mouse (x,y)
-
         Vector3 mousePoint = Input.mousePosition;
 
-
         // z coordinate of game object on screen
-
         mousePoint.z = mZCoord;
 
-
         // Convert it to world points
-
         return Camera.main.ScreenToWorldPoint(mousePoint);
-
     }
 
 
-    void OnMouseDrag()
+    void OnMouseDrag(){
 
-    {
-        
         transform.position = GetMouseAsWorldPoint() + mOffset;
 
     }
 
-    public Vector3 targetPosition;
-    public float snapDistance = 0.1f;
-    public List<Transform> nodes = new List<Transform>();
+    // public Vector3 targetPosition;
+    // public float snapDistance = 0.1f;
+    // public List<Transform> nodes = new List<Transform>();
+
+    public List<PegSnap> _pegs = new List<PegSnap>();
+    // public PegSnap _peg;
+
     void OnMouseUp()
     {
-        
-        transform.position = GetMouseAsWorldPoint() + mOffset;
-        float smallestDistance = snapDistance;
-        foreach (Transform node in transform)
-        {
-            Debug.Log("Node:"+ node.name);
-            
-            if (Vector3.Distance(node.position, targetPosition) < smallestDistance)
-            {
-                
-                transform.position = node.position;
-                smallestDistance = Vector3.Distance(node.position, targetPosition);
+        foreach(PegSnap _peg in _pegs){
+            if(Vector3.Distance(transform.position, _peg.transform.position) < 0.5){
+                transform.position = _peg.transform.position;
             }
+        }
+
+        // if(Vector3.Distance(transform.position, _peg.transform.position) < 0.5){
+        //     transform.position = _peg.transform.position;
+        // }
+
+
+        // transform.position = GetMouseAsWorldPoint() + mOffset;
+        // float smallestDistance = snapDistance;
+        // foreach (Transform node in transform)
+        // {
+        //     Debug.Log("Node:"+ node.name);
             
-         }
+        //     if (Vector3.Distance(node.position, targetPosition) < smallestDistance)
+        //     {
+                
+        //         transform.position = node.position;
+        //         smallestDistance = Vector3.Distance(node.position, targetPosition);
+        //     } 
+        //  }
+
+
     }
 
 }
