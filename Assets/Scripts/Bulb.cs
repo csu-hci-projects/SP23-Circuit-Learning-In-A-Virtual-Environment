f661dc9e00a878da3b50722ab992449a8d7d40aa
Freya  LaLuna <freya.laluna@colostate.edu>
2023-03-30 20:24:26 -0600

    Added bulb functionality

--- Assets/Scripts/Bulb.cs
+++ Assets/Scripts/Bulb.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class Bulb : CircuitComponent
 {
-    protected override void Update() { }
+    [SerializeField] Light bulbLight;
+    protected override void Update() { 
+        /*
+        if(voltage > 0, and this component is connected on both ends)
+            bulbLight.enabled = true;
+            bulbLight.intensity = (circuit voltage/circuit resistance) * some scaling coefficient;
+        */
+
+        //If the Bulb is on the pegboard, and both ends are connected, turn the light on
+        if((startPeg != null && endPeg != null) && 
+            (startPeg.attachedComponents.Count == 2 && endPeg.attachedComponents.Count == 2)){
+            bulbLight.intensity = 1;
+            bulbLight.enabled = true; 
+        } else {
+            bulbLight.intensity = 0;
+            bulbLight.enabled = false;
+        }    
+    }
 }
