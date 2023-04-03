1edfad6d12b1280110f34a62dd1ce99b333eec70
Stoophernii <71806946+Stoophernii@users.noreply.github.com>
2023-04-03 14:46:37 -0600

    Removed Start() and Update() in PegSnap
    Unity methods not used. PegSnap is mostly used for data manipulation not actual object interactions.

--- Assets/Scripts/PegSnap.cs
+++ Assets/Scripts/PegSnap.cs
@@ -11,18 +11,6 @@ public class PegSnap : MonoBehaviour
     public int row;
     public int col;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void connect(CircuitComponent connected)
     {
         attachedComponents.Add(connected);
