873d4d8126c0a59bf51697627e7005955c7713a2
Stoophernii <71806946+Stoophernii@users.noreply.github.com>
2023-04-11 15:13:53 -0600

    ItemTray and component duping in MouseDrag
    MouseDrag now uses CircuitComponent's direction enum instead of its own boolean. Effect of mouse dragging now changes to dupe tray items (normal components behave the same)

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-
 using System.Collections.Generic;
 
 using UnityEngine;
 
 public class MouseDrag : MonoBehaviour
 {
+    [SerializeField] private TrayItem _trayItem;
     [SerializeField] private CircuitLab _lab;
     [SerializeField] private CircuitComponent _thisComponent;
 
@@ -17,16 +16,25 @@ public class MouseDrag : MonoBehaviour
     private Vector3 _mouseOffset;
     private float _height;
 
+    public static int numComponentsCreated = 0;
+
     void Start()
     {
         _lab = (CircuitLab)FindObjectOfType(typeof(CircuitLab));
+        _trayItem = gameObject.GetComponent<TrayItem>();
         mainCamera = Camera.main;
-        // CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
     }
 
     void OnMouseDrag()
     {
-        moveToMouse();
+        if (_trayItem != null)
+        {
+            moveToMouse(_trayItem.dupedComponent);
+        }
+        else
+        {
+            moveToMouse(gameObject);
+        }
     }
 
     void OnMouseDown()
@@ -37,36 +45,35 @@ public class MouseDrag : MonoBehaviour
         Vector3 mousePoint = Input.mousePosition;
         mousePoint.z = _height;
         _mouseOffset = mainCamera.WorldToScreenPoint(gameObject.transform.position) - mousePoint;
-        
-        if (_thisComponent.isSnapped())
+
+        if (_trayItem != null)
         {
-            _thisComponent.disconnect();
+            _trayItem.duplicateComponent();
+            _lab.addCircuitComponent(_trayItem.dupedComponent.GetComponent<CircuitComponent>());
+        }
+        else
+        {
+
+            if (_thisComponent.isSnapped())
+            {
+                _thisComponent.disconnect();
+            }
         }
     }
 
 
     void OnMouseUp()
     {
-        _height = DEFAULT_HEIGHT;
-        moveToMouse();
-
-        foreach(PegSnap peg in _lab.board.getAllPegs())
+        if (_trayItem != null)
         {
-            if(Vector3.Distance(gameObject.transform.position, peg.transform.position) < SNAP_DISTANCE && !peg.blocked){
-                gameObject.transform.position = peg.transform.position;
-
-                if (peg.blocked)
-                {
-                    Debug.Log("Can't connect to blocked peg");
-                    break;
-                }
-
-                List<PegSnap> pegsToConnect = new List<PegSnap>() { peg, nextPegOver(peg) };
-                _thisComponent.connect(pegsToConnect);
-            }
+            _trayItem.dupedComponent.GetComponent<MouseDrag>().trySnapping();
+            _trayItem.dupedComponent = null;
+        }
+        else
+        {
+            trySnapping();
         }
 
-        _lab.constructCircuits();
     }
 
     private PegSnap nextPegOver(PegSnap given)
@@ -92,12 +99,41 @@ public class MouseDrag : MonoBehaviour
         return otherPeg;
     }
 
-    private void moveToMouse()
+    private void moveToMouse(GameObject objectToMove)
     {
         // get new mouse position and add offset, then move object to new location
         Vector3 mousePos = new Vector3(Input.mousePosition.x + _mouseOffset.x, Input.mousePosition.y + _mouseOffset.y, _height);
         Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
-        transform.position = worldPos;
+        objectToMove.transform.position = worldPos;
+    }
+
+    public void trySnapping()
+    {
+        _height = DEFAULT_HEIGHT;
+
+        moveToMouse(gameObject);
+
+        foreach (PegSnap peg in _lab.board.getAllPegs())
+        {
+            if (Vector3.Distance(transform.position, peg.transform.position) < SNAP_DISTANCE && !peg.blocked)
+            {
+                transform.position = peg.transform.position;
+
+                if (peg.blocked)
+                {
+                    Debug.Log("Can't connect to blocked peg");
+                    break;
+                }
+
+                List<PegSnap> pegsToConnect = new List<PegSnap>() { peg, nextPegOver(peg) };
+                _thisComponent.connect(pegsToConnect);
+                _lab.constructCircuits();
+
+                return;
+            }
+        }
+
+        Destroy(gameObject);
     }
 
 }
