d4e06d34b2978ffe79aead37516a197b5991867c
Stoophernii <71806946+Stoophernii@users.noreply.github.com>
2023-04-11 14:59:55 -0600

    TrayItem class
    class that handles special functionality of tray items. Added when each item is created in ItemTray.cs

---
+++ Assets/Scripts/TrayItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrayItem : MonoBehaviour
+{
+    public GameObject dupedComponent;
+    public static int numComponentsDuped = 0;
+
+    public void duplicateComponent()
+    {
+        dupedComponent = GameObject.Instantiate(gameObject, gameObject.transform.localPosition, gameObject.transform.localRotation);
+        dupedComponent.transform.localPosition = gameObject.transform.position;
+        dupedComponent.transform.localRotation = gameObject.transform.rotation;
+
+        string componentName = dupedComponent.name.Split("_")[0].Split("(")[0];
+        dupedComponent.name = componentName + "_" + dupedComponent.GetComponent<CircuitComponent>().direction + " (" + numComponentsDuped++ + ")";
+
+        Component.Destroy(dupedComponent.GetComponent<TrayItem>());
+    }
+}
